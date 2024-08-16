d252905879a1667e50065b704ec1c53028d6e431
Sasha <sasha.ruchits.06@gmail.com>
2024-08-16 20:56:07 +0300

    Initial

---
+++ Postcrossing.Api/Controllers/ExamController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Postcrossing.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ExamController : ControllerBase
+{
+    [HttpGet]
+    [Route("All")]
+    public Task<IActionResult> Get(CancellationToken cancellationToken)
+    {
+        return Task.FromResult<IActionResult>(Ok( "string"));
+    }
+}
