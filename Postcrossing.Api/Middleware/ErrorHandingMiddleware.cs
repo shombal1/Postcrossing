b25094412051f53cd3bb4f64dbd7fed73cf6d1f3
Sasha <sasha.ruchits.06@gmail.com>
2024-08-21 22:32:52 +0300

    Add error handing middleware

---
+++ Postcrossing.Api/Middleware/ErrorHandingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Postcrossing.Api.Middleware;
+
+public class ErrorHandingMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context,ProblemDetailsFactory problemDetailsFactory)
+    {
+        try
+        {
+            await next.Invoke(context);
+        }
+        catch (Exception e)
+        {
+            ProblemDetails problemDetails;
+            
+            switch (e)
+            {
+                case ValidationException validationException:
+                    problemDetails = problemDetailsFactory.CreateFrom(context, validationException);
+                    break;
+                default:
+                    problemDetails = problemDetailsFactory.CreateProblemDetails(
+                        context,
+                        StatusCodes.Status500InternalServerError,
+                        "Unhandled exception",
+                        detail: e.Message);
+                    break;
+            }
+
+            context.Response.StatusCode = problemDetails.Status ?? StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsJsonAsync(problemDetails, problemDetails.GetType());
+        }
+    }
+}
---
+++ Postcrossing.Api/Middleware/ProblemDetailsFactoryExtension.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Postcrossing.Api.Middleware;
+
+public static class ProblemDetailsFactoryExtension
+{
+    public static ProblemDetails CreateFrom(this ProblemDetailsFactory problemDetailsFactory, HttpContext context,
+        ValidationException validationException)
+    {
+        ModelStateDictionary stateDictionary = new ModelStateDictionary();
+        foreach (var error in validationException.Errors)
+        {
+            stateDictionary.AddModelError(error.PropertyName,error.ErrorMessage);
+        }
+        return problemDetailsFactory.CreateValidationProblemDetails(
+            context,stateDictionary,StatusCodes.Status400BadRequest,"Bad request");
+    }
+}
